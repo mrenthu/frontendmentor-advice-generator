082a83924a546d4a36c85f1d4ecf9c02e8ab5c42
Mr. Enthusiast <mrenthu@int.pl>
2022-12-19 17:12:33 +0100

    2022.12.19.0: Second Version, Added logic, a few changes to class names and moved images to src dir

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
-console.log("Welcome!")
+const el_advice_number = document.querySelector(".advice_number")
+const el_advice_content = document.querySelector(".advice_content")
+const el_randomize_button = document.querySelector(".randomize_button")
+
+const API_URL = "https://api.adviceslip.com/advice"
+
+const getDataFromApi = async () => {
+	const response = await fetch(API_URL)
+		.then(data => data.json())
+
+	if (response.ok == false) {
+		const message = `An error has occured: ${response.status}`;
+
+		throw new Error(message);
+	}
+
+	return response
+}
+
+const htmlGeneration = () => {
+	const response = getDataFromApi();
+
+	response.then(data => {
+		let { id, advice } = data.slip
+
+		el_advice_number.innerHTML = `Advice #${id}`
+		el_advice_content.innerHTML = `${advice}`
+	})
+}
+
+el_randomize_button.addEventListener("click", htmlGeneration)
+
+document.body.addEventListener("load", htmlGeneration())
